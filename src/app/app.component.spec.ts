85f6c8d966d4f7c7398a863b987b0fc21a814f5c
Charlotte Liu <liu.charlotte@gmail.com>
2020-05-24 13:59:25 +0800

    initial file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang-jwt-auth1'`, () => {
+  it(`should have as title 'Angular8JwtAuth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ang-jwt-auth1');
+    expect(app.title).toEqual('Angular8JwtAuth');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ang-jwt-auth1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular8JwtAuth app is running!');
   });
 });
